65703275b23e5c4ecc1bad747c11df42145577e3
Shijin Zhang <1364226740@qq.com>
2024-01-22 22:12:10 +0800

    Change-the test code for the related part of admin

--- fyp-springboot-code/src/test/java/com/example/fypspringbootcode/FypSpringbootCodeApplicationTests.java
+++ fyp-springboot-code/src/test/java/com/example/fypspringbootcode/FypSpringbootCodeApplicationTests.java
@@ -1,21 +1,18 @@
 package com.example.fypspringbootcode;
 
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
 import com.example.fypspringbootcode.entity.Admin;
-import com.example.fypspringbootcode.exception.ServiceException;
 import com.example.fypspringbootcode.mapper.AdminMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DuplicateKeyException;
 
 @SpringBootTest
 @Slf4j //用于异常错误抛出 下方log.error
 class FypSpringbootCodeApplicationTests {
 
-    private static final String DEFAULT_PASS = "123456";
+    private static final String DEFAULT_PASS = "zsj123456";
     private static final String PASS_SALT = "Dovis"; //密码盐
 
     @Autowired
@@ -23,24 +20,6 @@ class FypSpringbootCodeApplicationTests {
     @Test
     public Admin getById(Integer id) {
         return adminMapper.selectById(id);
-    }
-    @Test
-    public void save() {
-        Admin admin = new Admin(1,"admin","zsj123456");
-        //如果admin对象的密码属性为空 则设置默认密码 123
-        //hutool包下的工具类StrUtil
-        if (StrUtil.isBlank(admin.getPassword())) {
-            admin.setPassword(DEFAULT_PASS);
-        }
-        //hutool包下的工具类SecureUtil
-        admin.setPassword(securePass(admin.getPassword()));  // 设置md5加密，加盐
-        try {
-            //可能发生数据库加入重复admin用户名值发生的未捕获的异常  save方法可能会失败
-            adminMapper.insert(admin);
-        } catch (DuplicateKeyException e) {
-            log.error("数据插入失败， username:{}", admin.getAdminName(), e);
-            throw new ServiceException("用户名重复");
-        }
     }
 
     @Test
--- fyp-springboot-code/src/test/java/com/example/fypspringbootcode/tests/AdminTest.java
+++ fyp-springboot-code/src/test/java/com/example/fypspringbootcode/tests/AdminTest.java
@@ -1,16 +1,13 @@
 package com.example.fypspringbootcode.tests;
 
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.fypspringbootcode.entity.Admin;
-import com.example.fypspringbootcode.exception.ServiceException;
 import com.example.fypspringbootcode.mapper.AdminMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.stereotype.Service;
 
 /**
  * @title:FinalYearProjectCode
@@ -23,7 +20,7 @@ import org.springframework.stereotype.Service;
 @Slf4j //用于异常错误抛出 下方log.error
 public class AdminTest {
 
-    private static final String DEFAULT_PASS = "123456";
+    private static final String DEFAULT_PASS = "zsj123456";
     private static final String PASS_SALT = "Dovis"; //密码盐
 
     @Autowired
@@ -32,23 +29,29 @@ public class AdminTest {
     public void getById() {
         System.out.println(adminMapper.selectById(1));
     }
+//    @Test
+//    public void save() {
+//        Admin admin = new Admin(4,"20104636@mail.wit.ie","Password123!");
+//        //如果admin对象的密码属性为空 则设置默认密码 123
+//        //hutool包下的工具类StrUtil
+//        if (StrUtil.isBlank(admin.getPassword())) {
+//            admin.setPassword(DEFAULT_PASS);
+//        }
+//        //hutool包下的工具类SecureUtil
+//        admin.setPassword(securePass(admin.getPassword()));  // 设置md5加密，加盐
+//        try {
+//            //可能发生数据库加入重复admin用户名值发生的未捕获的异常  save方法可能会失败
+//            adminMapper.insert(admin);
+//        } catch (DuplicateKeyException e) {
+//            log.error("数据插入失败， username:{}", admin.getAdminName(), e);
+//            throw new ServiceException("用户名重复");
+//        }
+//    }
     @Test
-    public void save() {
-        Admin admin = new Admin(3,"admin3","zsj123456");
-        //如果admin对象的密码属性为空 则设置默认密码 123
-        //hutool包下的工具类StrUtil
-        if (StrUtil.isBlank(admin.getPassword())) {
-            admin.setPassword(DEFAULT_PASS);
-        }
-        //hutool包下的工具类SecureUtil
-        admin.setPassword(securePass(admin.getPassword()));  // 设置md5加密，加盐
-        try {
-            //可能发生数据库加入重复admin用户名值发生的未捕获的异常  save方法可能会失败
-            adminMapper.insert(admin);
-        } catch (DuplicateKeyException e) {
-            log.error("数据插入失败， username:{}", admin.getAdminName(), e);
-            throw new ServiceException("用户名重复");
-        }
+    public void login() {
+        QueryWrapper<Admin> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("admin_name", "admin1");
+        System.out.println(adminMapper.selectOne(queryWrapper));
     }
 
     @Test
