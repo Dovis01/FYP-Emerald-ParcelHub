635077d5079cc022444d4a128c00b06bd5722676
Shijin Zhang <1364226740@qq.com>
2024-02-08 00:20:48 +0800

    Improve-exception prompt info and add error code fields

---
+++ fyp-springboot-code/src/main/java/com/example/fypspringbootcode/common/ErrorCodeList.java
@@ -0,0 +1,14 @@
+package com.example.fypspringbootcode.common;
+
+/**
+ * @title:FinalYearProjectCode
+ * @description:<TODO description class purpose>
+ * @author: Shijin Zhang
+ * @version:1.0.0
+ * @create:05/02/2024 22:31
+ **/
+public class ErrorCodeList {
+    public static final String ERROR_CODE_401 = "401";
+    public static final String ERROR_CODE_404 = "404";
+    public static final String ERROR_CODE_500 = "500";
+}
--- fyp-springboot-code/src/main/java/com/example/fypspringbootcode/exception/ExceptionHandle.java
+++ fyp-springboot-code/src/main/java/com/example/fypspringbootcode/exception/ExceptionHandle.java
@@ -15,7 +15,7 @@ public class ExceptionHandle {
     public Result serviceExceptionError(ServiceException e) {
     //将后端实际运行出现的异常作为Throwable cause 捕获抛到这里被log.error所打印出来.
     //统一的对具体运行出现的异常放在这里进行log.error打印出来
-        log.error("业务异常", e);
+        log.error("System business is error", e);
         String code = e.getCode();
         if (StrUtil.isNotBlank(code)) {
             return Result.error(code, e.getMessage());
@@ -26,8 +26,8 @@ public class ExceptionHandle {
     //没有抛出自定义异常类，属于自定义异常类管理范围之外的错误，系统运行中发生代码错误会触发该类
     @ExceptionHandler(value = Exception.class)
     public Result exceptionError(Exception e) {
-        log.error("系统错误", e);
-        return Result.error("系统错误");
+        log.error("System is error", e);
+        return Result.error("System is error, please contact the administrator.");
     }
 
 }
